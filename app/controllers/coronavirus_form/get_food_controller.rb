b117abb171e6dbd3e576fd3a62f4e34b39b9b7d9
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-04-08 10:48:21 +0100

    Add "are you able to get food" question

---
+++ app/controllers/coronavirus_form/get_food_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class CoronavirusForm::GetFoodController < ApplicationController
+  def submit
+    @form_responses = {
+      get_food: strip_tags(params[:get_food]).presence,
+    }
+
+    invalid_fields = validate_radio_field(
+      controller_name,
+      radio: @form_responses[:get_food],
+    )
+
+    if invalid_fields.any?
+      flash.now[:validation] = invalid_fields
+      log_validation_error(invalid_fields)
+      render controller_path
+    else
+      update_session_store
+      #redirect_to nil
+    end
+  end
+
+private
+
+  def update_session_store
+    session[:get_food] = @form_responses[:get_food]
+  end
+
+  def previous_path; end
+end

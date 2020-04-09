6a57940888c2fd67b5a4c3123919e6640c5c8ce0
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2020-04-09 14:58:48 +0100

    Create table for response storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_09_135543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "form_responses", force: :cascade do |t|
+    t.jsonb "form_response", default: {}, null: false
+    t.datetime "created_at", null: false
+  end
 
 end

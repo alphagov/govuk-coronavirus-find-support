e19e30218a68f57667c48c415e814a158f06faf6
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-04-09 21:18:20 +0100

    Remove skip_before_action from session controller
    We thought this was defined in the Application Controller
    Turns out you can't skip something that's never been before-ed

--- app/controllers/coronavirus_form/session_controller.rb
+++ app/controllers/coronavirus_form/session_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class CoronavirusForm::SessionController < ApplicationController
-  skip_before_action :check_first_question
-
   def delete
     reset_session
     redirect_to "/"

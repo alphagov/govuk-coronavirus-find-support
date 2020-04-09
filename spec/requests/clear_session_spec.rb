10342228b4806745ca496f973eac65f49e90d43b
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-04-09 21:32:02 +0100

    Fix clear session tests

--- spec/requests/clear_session_spec.rb
+++ spec/requests/clear_session_spec.rb
@@ -1,20 +1,20 @@
 RSpec.describe "clear-session" do
+  let(:selected_no) { I18n.t("coronavirus_form.groups.help.questions.urgent_medical_help.options").last }
+
   describe "GET /clear-session" do
     context "with session data" do
-      before do
-        post afford_food_path, params: { afford_food: "Yes" }
-      end
-
       it "clears the session id" do
+        post urgent_medical_help_path, params: { urgent_medical_help: selected_no }
         initial_session_id = session["session_id"]
         get clear_session_path
         expect(session["session_id"]).to_not eq(initial_session_id)
       end
 
       it "clears responses held in the session" do
-        expect(session["afford_food"]).to eq("Yes")
+        post urgent_medical_help_path, params: { urgent_medical_help: selected_no }
+        expect(session[:urgent_medical_help]).to eq(selected_no)
         get clear_session_path
-        expect(session["afford_food"]).to be_nil
+        expect(session[:have_you_been_made_unemployed]).to be_nil
       end
 
       it "redirects the user to root" do

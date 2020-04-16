9f0b1d4a1c9f11a18cbf8f0cc78c5716281e011b
Huw Diprose <mail@huwdiprose.co.uk>
2020-04-16 13:46:54 +0100

    Clear session can take an optional redirect parameter
    Change clear session redirect to a strategy that prevents [Open Redirect Attacks](https://cwe.mitre.org/data/definitions/601.html)
    
    Params are strings not booleans
    
    Commit suggested change from Richard Towers

--- app/controllers/coronavirus_form/session_controller.rb
+++ app/controllers/coronavirus_form/session_controller.rb
@@ -3,6 +3,10 @@
 class CoronavirusForm::SessionController < ApplicationController
   def delete
     reset_session
-    redirect_to "/"
+    if params[:ext_r] == "true"
+      redirect_to I18n.t("leave_this_website.link_redirect_to")
+    else
+      redirect_to "/"
+    end
   end
 end

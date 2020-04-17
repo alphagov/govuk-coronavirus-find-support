de6fc87389f1ba59ffd76bab9faec3c4d7c3aa62
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-04-17 16:02:01 +0100

    Skip deleted questions
    We recently removed a question.  Since some users had already begun
    their journey through the form, their `questions_to_ask` session array
    contained the removed question's key and they were redirected to that
    path, even though it no longer exists.
    
    This performs an intersection of questions the user should be asked, and
    the questions available in the app each time the next or previous
    question is being determined.  That will prevent redirection to
    non-existent routes.

--- spec/helpers/questions_helper_spec.rb
+++ spec/helpers/questions_helper_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe QuestionsHelper, type: :helper do
   before do
-    allow(helper).to receive(:questions_to_ask).and_return(%w(question_1 question_2))
+    allow(helper).to receive(:session_questions).and_return(%w(get_food afford_food))
   end
 
   describe "#determine_user_questions" do
@@ -21,29 +21,47 @@ RSpec.describe QuestionsHelper, type: :helper do
 
   describe "#next_question" do
     it "returns the next question key" do
-      expect(helper.next_question("question_1")).to eq("question_2")
+      expect(helper.next_question("get_food")).to eq("afford_food")
     end
 
     it "returns the final compulsory question for the final item" do
-      expect(helper.next_question("question_2")).to eq("able_to_leave")
+      expect(helper.next_question("afford_food")).to eq("able_to_leave")
     end
 
     it "returns the first question for the need help with question" do
-      expect(helper.next_question("need_help_with")).to eq("question_1")
+      expect(helper.next_question("need_help_with")).to eq("get_food")
     end
   end
 
   describe "#previous_question" do
     it "returns the previous question key" do
-      expect(helper.previous_question("question_2")).to eq("question_1")
+      expect(helper.previous_question("afford_food")).to eq("get_food")
     end
 
     it "returns the filter question page key for the first item" do
-      expect(helper.previous_question("question_1")).to eq("need_help_with")
+      expect(helper.previous_question("get_food")).to eq("need_help_with")
     end
 
     it "returns the last question for the able to leave question" do
-      expect(helper.previous_question("able_to_leave")).to eq("question_2")
+      expect(helper.previous_question("able_to_leave")).to eq("afford_food")
+    end
+  end
+
+  describe "#questions_to_ask" do
+    it "returns questions to ask the user" do
+      expect(helper.questions_to_ask).to eq(%w(get_food afford_food))
+    end
+
+    it "does not include a question that has been removed since the user made their selection" do
+      allow(helper).to receive(:all_questions).and_return(%w(get_food))
+
+      expect(helper.questions_to_ask).to eq(%w(get_food))
+    end
+
+    it "does not include a question that has been added since the user made their selection" do
+      allow(helper).to receive(:all_questions).and_return(%w(get_food afford_food question_3))
+
+      expect(helper.questions_to_ask).to eq(%w(get_food afford_food))
     end
   end
 end

b490738fb80a6b55e730809f2c7564a1412782f0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-04-17 18:08:58 +0100

    Update data export tests and delimiter
    Changes the delimiter in the csv file from `,` to `|`. This is because
    many of the title and option label strings already contain commas, so
    the CSV generator was wrapping this strings (and only these strings) in
    double quotes and the tests were compensating for this but also wrapping
    those strings. But if the text ever changes and the comma is removed,
    the tests would still need to be updated, which is the situation we are
    trying to avoid.
    
    Also updates all of tests to get their string values from the I18n file.

--- spec/requests/data_export_spec.rb
+++ spec/requests/data_export_spec.rb
@@ -3,23 +3,68 @@ RSpec.describe "data-export" do
   let(:end_date) { "2020-04-15" }
 
   before do
-    FormResponse.create(form_response: { able_to_leave: "Yes", get_food: "Yes" }, created_at: "2020-04-10 10:00:00")
-    FormResponse.create(form_response: { able_to_leave: "Yes", get_food: "No" }, created_at: "2020-04-10 10:00:00")
-    FormResponse.create(form_response: { able_to_leave: "Yes", get_food: "Yes" }, created_at: "2020-04-11 10:00:00")
-    FormResponse.create(form_response: { able_to_leave: "No", get_food: "No" }, created_at: "2020-04-11 10:00:00")
+    FormResponse.create(
+      form_response: {
+       able_to_leave: I18n.t("coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_yes.label"),
+       get_food: I18n.t("coronavirus_form.groups.getting_food.questions.get_food.options.option_yes.label"),
+      },
+      created_at: "2020-04-10 10:00:00",
+     )
+    FormResponse.create(
+      form_response: {
+        able_to_leave: I18n.t("coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_yes.label"),
+        get_food: I18n.t("coronavirus_form.groups.getting_food.questions.get_food.options.option_no.label"),
+      },
+      created_at: "2020-04-10 10:00:00",
+    )
+    FormResponse.create(
+      form_response: {
+        able_to_leave: I18n.t("coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_yes.label"),
+        get_food: I18n.t("coronavirus_form.groups.getting_food.questions.get_food.options.option_yes.label"),
+      },
+      created_at: "2020-04-11 10:00:00",
+    )
+    FormResponse.create(
+      form_response: {
+        able_to_leave: I18n.t("coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_other.label"),
+        get_food: I18n.t("coronavirus_form.groups.getting_food.questions.get_food.options.option_no.label"),
+      },
+      created_at: "2020-04-11 10:00:00",
+    )
   end
 
   describe "GET /able-to-leave" do
     let(:expected_lines) do
       [
-        %{question,answer,date,count},
-        %{"Are you able to leave your home for food, medicine, or health reasons?",Yes,2020-04-10,2},
-        %{"Are you able to leave your home for food, medicine, or health reasons?",Yes,2020-04-11,1},
-        %{"Are you able to leave your home for food, medicine, or health reasons?",No,2020-04-11,1},
-        %{Are you able to get food?,Yes,2020-04-10,1},
-        %{Are you able to get food?,No,2020-04-10,1},
-        %{Are you able to get food?,Yes,2020-04-11,1},
-        %{Are you able to get food?,No,2020-04-11,1},
+        "question|answer|date|count",
+        "#{I18n.t('coronavirus_form.groups.leave_home.questions.able_to_leave.title')}|" \
+          "#{I18n.t('coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_yes.label')}|" \
+          "2020-04-10|" \
+          "2",
+        "#{I18n.t('coronavirus_form.groups.leave_home.questions.able_to_leave.title')}|" \
+          "#{I18n.t('coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_yes.label')}|" \
+          "2020-04-11|" \
+          "1",
+        "#{I18n.t('coronavirus_form.groups.leave_home.questions.able_to_leave.title')}|" \
+          "#{I18n.t('coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_other.label')}|" \
+          "2020-04-11|" \
+          "1",
+        "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.title')}|" \
+          "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.options.option_yes.label')}|" \
+          "2020-04-10|" \
+          "1",
+        "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.title')}|" \
+          "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.options.option_no.label')}|" \
+          "2020-04-10|" \
+          "1",
+        "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.title')}|" \
+          "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.options.option_yes.label')}|" \
+          "2020-04-11|" \
+          "1",
+        "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.title')}|" \
+          "#{I18n.t('coronavirus_form.groups.getting_food.questions.get_food.options.option_no.label')}|" \
+          "2020-04-11|" \
+          "1",
       ]
     end
 

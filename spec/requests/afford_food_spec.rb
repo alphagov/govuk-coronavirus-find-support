95c39a028ab96aa7716b4f8c71a5c586ca8bc6f6
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-04-17 17:48:12 +0100

    Update afford food tests

--- spec/requests/afford_food_spec.rb
+++ spec/requests/afford_food_spec.rb
@@ -1,11 +1,13 @@
 RSpec.describe "afford-food" do
+  let(:options) { I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.options") }
+  let(:selected_option) { options.keys.sample }
+  let(:selected_option_text) { I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.options.#{selected_option}.label") }
+
   before do
     allow_any_instance_of(QuestionsHelper).to receive(:questions_to_ask).and_return(%w(afford_food feel_safe))
   end
 
   describe "GET /afford-food" do
-    let(:selected_option) { I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.options").sample }
-
     context "without any questions to ask in the session data" do
       before do
         allow_any_instance_of(QuestionsHelper).to receive(:questions_to_ask).and_return(nil)
@@ -23,22 +25,22 @@ RSpec.describe "afford-food" do
         visit afford_food_path
 
         expect(page.body).to have_content(I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.title"))
-        I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.options").each do |option|
-          expect(page.body).to have_content(option)
+        I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.options").each do |_, option|
+          expect(page.body).to have_content(option[:label])
         end
       end
     end
 
     context "with session data" do
       before do
-        page.set_rack_session(afford_food: selected_option)
+        page.set_rack_session(afford_food: selected_option_text)
       end
 
       it "shows the form without prefilled response" do
         visit afford_food_path
 
         expect(page.body).to have_content(I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.title"))
-        expect(page.find("input#option_#{selected_option.parameterize.underscore}")).not_to be_checked
+        expect(page.find("input##{selected_option}")).not_to be_checked
       end
     end
 
@@ -56,16 +58,14 @@ RSpec.describe "afford-food" do
   end
 
   describe "POST /afford-food" do
-    let(:selected_option) { I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.options").sample }
-
     it "updates the session store" do
-      post afford_food_path, params: { afford_food: selected_option }
+      post afford_food_path, params: { afford_food: selected_option_text }
 
-      expect(session[:afford_food]).to eq(selected_option)
+      expect(session[:afford_food]).to eq(selected_option_text)
     end
 
     it "redirects to the next question" do
-      post afford_food_path, params: { afford_food: selected_option }
+      post afford_food_path, params: { afford_food: selected_option_text }
 
       expect(response).to redirect_to(controller: "feel_safe", action: "show")
     end

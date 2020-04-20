a2f4532522fbf7bea81f651b98be71e0a4904ef3
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-04-20 11:52:57 +0100

    Update selected answers in feature tests

--- spec/support/fill_in_the_form_steps.rb
+++ spec/support/fill_in_the_form_steps.rb
@@ -8,7 +8,7 @@ module FillInTheFormSteps
   def and_does_not_need_urgent_medical_help
     expect(page).to have_content(I18n.t("coronavirus_form.groups.help.questions.urgent_medical_help.title"))
 
-    choose "No"
+    choose I18n.t("coronavirus_form.groups.help.questions.urgent_medical_help.options.option_no.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -31,7 +31,7 @@ module FillInTheFormSteps
   def and_feels_unsafe_where_they_live
     expect(page).to have_content(I18n.t("coronavirus_form.groups.feeling_unsafe.questions.feel_safe.title"))
 
-    choose "No"
+    choose I18n.t("coronavirus_form.groups.feeling_unsafe.questions.feel_safe.options.option_no.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -39,7 +39,7 @@ module FillInTheFormSteps
   def and_is_finding_it_hard_to_afford_rent_mortgage_bills
     expect(page).to have_content(I18n.t("coronavirus_form.groups.paying_bills.questions.afford_rent_mortgage_bills.title"))
 
-    choose "Yes"
+    choose I18n.t("coronavirus_form.groups.paying_bills.questions.afford_rent_mortgage_bills.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -47,7 +47,7 @@ module FillInTheFormSteps
   def and_is_finding_it_hard_to_afford_food
     expect(page).to have_content(I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.title"))
 
-    choose "Yes"
+    choose I18n.t("coronavirus_form.groups.getting_food.questions.afford_food.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -55,7 +55,7 @@ module FillInTheFormSteps
   def and_is_unable_to_get_food
     expect(page).to have_content(I18n.t("coronavirus_form.groups.getting_food.questions.get_food.title"))
 
-    choose "No"
+    choose I18n.t("coronavirus_form.groups.getting_food.questions.get_food.options.option_no.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -63,7 +63,7 @@ module FillInTheFormSteps
   def and_has_been_told_to_stop_working
     expect(page).to have_content(I18n.t("coronavirus_form.groups.being_unemployed.questions.have_you_been_made_unemployed.title"))
 
-    choose "Yes, I’ve been made unemployed, or might be soon"
+    choose I18n.t("coronavirus_form.groups.being_unemployed.questions.have_you_been_made_unemployed.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -71,7 +71,7 @@ module FillInTheFormSteps
   def and_is_off_work_because_ill_or_self_isolating
     expect(page).to have_content(I18n.t("coronavirus_form.groups.being_unemployed.questions.are_you_off_work_ill.title"))
 
-    choose "Yes"
+    choose I18n.t("coronavirus_form.groups.being_unemployed.questions.are_you_off_work_ill.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -79,7 +79,7 @@ module FillInTheFormSteps
   def and_is_self_employed_or_a_sole_trader
     expect(page).to have_content(I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.title"))
 
-    choose "Yes"
+    choose I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -87,7 +87,7 @@ module FillInTheFormSteps
   def and_is_worried_about_going_to_work_because_of_living_with_someone_vulnerable
     expect(page).to have_content(I18n.t("coronavirus_form.groups.going_in_to_work.questions.living_with_vulnerable.title"))
 
-    choose "Yes"
+    choose I18n.t("coronavirus_form.groups.going_in_to_work.questions.living_with_vulnerable.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -95,7 +95,7 @@ module FillInTheFormSteps
   def and_has_nowhere_to_live
     expect(page).to have_content(I18n.t("coronavirus_form.groups.somewhere_to_live.questions.have_somewhere_to_live.title"))
 
-    choose "No"
+    choose I18n.t("coronavirus_form.groups.somewhere_to_live.questions.have_somewhere_to_live.options.option_no.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -103,7 +103,7 @@ module FillInTheFormSteps
   def and_has_been_evicted
     expect(page).to have_content(I18n.t("coronavirus_form.groups.somewhere_to_live.questions.have_you_been_evicted.title"))
 
-    choose "Yes"
+    choose I18n.t("coronavirus_form.groups.somewhere_to_live.questions.have_you_been_evicted.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -111,7 +111,7 @@ module FillInTheFormSteps
   def and_is_worried_about_mental_health
     expect(page).to have_content(I18n.t("coronavirus_form.groups.mental_health.questions.mental_health_worries.title"))
 
-    choose "Yes, I am"
+    choose I18n.t("coronavirus_form.groups.mental_health.questions.mental_health_worries.options.option_yes.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -119,7 +119,7 @@ module FillInTheFormSteps
   def and_is_not_able_to_leave_home_if_absolutely_necessary
     expect(page).to have_content(I18n.t("coronavirus_form.groups.leave_home.questions.able_to_leave.title"))
 
-    choose "I should not leave home because I have coronavirus symptoms, or someone in my household does"
+    choose I18n.t("coronavirus_form.groups.leave_home.questions.able_to_leave.options.option_has_symptoms.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end

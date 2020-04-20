69dddbd8eea0e82e890e4edd66cff741e7aeac50
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-04-20 10:30:46 +0100

    Do not show working questions when self employed
    When a user tells us they are self employed, we do not need to ask them
    any further questions about work.

--- spec/support/fill_in_the_form_steps.rb
+++ spec/support/fill_in_the_form_steps.rb
@@ -28,6 +28,14 @@ module FillInTheFormSteps
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
 
+  def and_needs_help_with_being_unemployed
+    expect(page).to have_content(I18n.t("coronavirus_form.groups.filter_questions.questions.need_help_with.title"))
+
+    check I18n.t("coronavirus_form.groups.being_unemployed.title")
+
+    click_on I18n.t("coronavirus_form.submit_and_next")
+  end
+
   def and_feels_unsafe_where_they_live
     expect(page).to have_content(I18n.t("coronavirus_form.groups.feeling_unsafe.questions.feel_safe.title"))
 
@@ -80,6 +88,14 @@ module FillInTheFormSteps
     expect(page).to have_content(I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.title"))
 
     choose I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.options.option_yes.label")
+
+    click_on I18n.t("coronavirus_form.submit_and_next")
+  end
+
+  def and_is_not_self_employed_or_a_sole_trader
+    expect(page).to have_content(I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.title"))
+
+    choose I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.options.option_no.label")
 
     click_on I18n.t("coronavirus_form.submit_and_next")
   end
@@ -142,10 +158,13 @@ module FillInTheFormSteps
     expect(page).to have_content(I18n.t("results_link.getting_food.get_food.title"))
   end
 
+  def they_are_provided_with_information_about_being_self_employed
+    expect(page).to have_content(I18n.t("results_link.being_unemployed.self_employed.title"))
+  end
+
   def they_are_provided_with_information_about_being_unemployed
     expect(page).to have_content(I18n.t("results_link.being_unemployed.have_you_been_made_unemployed.title"))
     expect(page).to have_content(I18n.t("results_link.being_unemployed.are_you_off_work_ill.title"))
-    expect(page).to have_content(I18n.t("results_link.being_unemployed.self_employed.title"))
   end
 
   def they_are_provided_with_information_about_going_in_to_work

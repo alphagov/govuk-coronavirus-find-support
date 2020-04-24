7b0c575d5985d537be9a20770f009e33177a0819
Rick Clare <rickclare@users.noreply.github.com>
2020-04-24 11:26:57 +0100

    Revert development config back to 'alphagov' defaults
    Revert some development changes (required for the *govwales* prototyping)
    back to the defaults, to avoid discrepancies between local development environments.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.file_watcher = ActiveSupport::FileUpdateChecker
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.analytics_tracking_id = "DEV-123456"
 end

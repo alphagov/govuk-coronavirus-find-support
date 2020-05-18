657e624f5a9c2f7e730e6c852b3e06793c321225
Huw Diprose <mail@huwdiprose.co.uk>
2020-05-18 17:04:02 +0100

    Add ContentImporter class
    Add ContentImporter and .import_results_links method()
    
    TOSQUASH: use blank? instead of nil?
    
    Add overwrite_locale_links()
    
    TOSQUASH: Stop overwriting subgroup titles

---
+++ lib/content_importer.rb
@@ -0,0 +1,53 @@
+require "csv"
+
+module ContentImporter
+module_function
+
+  def import_results_links(csv_path)
+    csv = CSV.read(csv_path, { headers: true })
+    output = csv.each_with_object({}) do |csv_row, results_links|
+      support_and_advice = csv_row.fetch("support_and_advice")
+      group_key = csv_row.fetch("group_key").to_sym
+      subgroup_key = csv_row.fetch("subgroup_key").to_sym
+
+      link_type = support_and_advice.downcase == "true" ? "support_and_advice" : "items"
+      results_links[group_key] = {} if results_links.dig(group_key).nil?
+      results_links[group_key][subgroup_key] = {} if results_links.dig(group_key, subgroup_key).nil?
+      if results_links.dig(group_key, subgroup_key, link_type).nil?
+        results_links[group_key][subgroup_key][link_type] = []
+      end
+
+      show_to_nations = csv_row.fetch("show_to_nations")
+      nations = show_to_nations.split(" OR ") if show_to_nations.present?
+
+      show_to_vulnerable_person = csv_row.fetch("show_to_vulnerable_person")
+      show_vulnerable_link = show_to_vulnerable_person.downcase == "true" if show_to_vulnerable_person.present?
+
+      results_links[group_key][subgroup_key][link_type] << {
+        id: sprintf("%04d", csv_row.fetch("id")), # Zero pad it!
+        text: csv_row.fetch("text"),
+        href: csv_row.fetch("href"),
+        show_to_nations: nations,
+        show_to_vulnerable_person: show_vulnerable_link,
+      }.reject { |_key, value| value.blank? }
+    end
+    output.deep_stringify_keys
+  end
+
+  def overwrite_locale_links(input_locale_path, locale_hash, output_locale_path = input_locale_path)
+    existing_locale_file = YAML.load_file(input_locale_path)
+    existing_locale_file["en"]["results_link"].keys.each do |group_key|
+      existing_locale_file["en"]["results_link"][group_key].keys.each do |subgroup_key|
+        subgroup = existing_locale_file["en"]["results_link"][group_key][subgroup_key]
+        subgroup["items"] = [] # Clear out old items in case we removed some on the sheet
+        subgroup["support_and_advice"] = [] unless subgroup["support_and_advice"].nil? # ditto s&a links
+        merged_locale = subgroup.merge(locale_hash[group_key][subgroup_key])
+        existing_locale_file["en"]["results_link"][group_key][subgroup_key] = merged_locale
+      end
+    end
+
+    File.open(output_locale_path, "w") do |file|
+      file.write(existing_locale_file.to_yaml)
+    end
+  end
+end

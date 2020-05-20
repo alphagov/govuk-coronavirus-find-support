635cc2a525d9cc1471fe5370dbc769fbc8e32acc
Mark Heath <31064750+markheathy@users.noreply.github.com>
2020-05-20 12:12:11 +0100

    footer pages and temp translations addition (#38)

---
+++ app/controllers/coronavirus_form/copyright_statement_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CoronavirusForm::CopyrightStatementController < ApplicationController
+  def show
+    @previous_page = return_path
+    super
+  end
+
+private
+
+  def return_path
+    request.referer.presence ? first_question_path : request.referer
+  end
+end

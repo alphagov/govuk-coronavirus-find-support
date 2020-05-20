305ed15f2f834d9f2a738c3923d42c7916eada0b
Huw Diprose <mail@huwdiprose.co.uk>
2020-05-20 14:51:29 +0100

    Change ContentExporter from Class to Module
    TIL, that was a bad habit of mine, shouldn't use a class if you're never going to create an instance, makes sense in retrospect

--- lib/content_exporter.rb
+++ lib/content_exporter.rb
@@ -1,44 +1,44 @@
 require "csv"
 
-class ContentExporter
-  class << self
-    def extract_results_links
-      result_links = I18n.t("results_link")
-      result_links.each_with_object([]) do |group, row|
-        group[1].each do |subgroup|
-          group_key = group[0]
-          subgroup_title = subgroup[1].fetch(:title)
-          subgroup[1].fetch(:items).each { |result| row << format_result_item_row(result, group_key, subgroup_title) }
-          subgroup[1].fetch(:support_and_advice_items, []).each do |result|
-            row << format_result_item_row(result, group_key, subgroup_title, true)
-          end
+module ContentExporter
+  extend self
+
+  def extract_results_links
+    result_links = I18n.t("results_link")
+    result_links.each_with_object([]) do |group, row|
+      group[1].each do |subgroup|
+        group_key = group[0]
+        subgroup_title = subgroup[1].fetch(:title)
+        subgroup[1].fetch(:items).each { |result| row << format_result_item_row(result, group_key, subgroup_title) }
+        subgroup[1].fetch(:support_and_advice_items, []).each do |result|
+          row << format_result_item_row(result, group_key, subgroup_title, true)
         end
       end
     end
+  end
 
-    def generate_results_link_csv
-      data = extract_results_links
-      column_names = data.map(&:keys).flatten.uniq
-      values = data.map(&:values)
-      CSV.generate do |csv|
-        csv << column_names.map(&:to_s)
-        values.each { |value| csv << value }
-      end
+  def generate_results_link_csv
+    data = extract_results_links
+    column_names = data.map(&:keys).flatten.uniq
+    values = data.map(&:values)
+    CSV.generate do |csv|
+      csv << column_names.map(&:to_s)
+      values.each { |value| csv << value }
     end
+  end
 
-  private
+private
 
-    def format_result_item_row(result, group_key, subgroup_title, support_and_advice = false)
-      groups = I18n.t("coronavirus_form.groups")
-      {
-        id: result.fetch(:id),
-        group_title: groups[group_key].fetch(:title),
-        subgroup_title: subgroup_title,
-        support_and_advice: support_and_advice,
-        text: result.fetch(:text),
-        href: result.fetch(:href, ""),
-        show_to_nations: result.fetch(:show_to_nations, []).join(" OR "),
-      }
-    end
+  def format_result_item_row(result, group_key, subgroup_title, support_and_advice = false)
+    groups = I18n.t("coronavirus_form.groups")
+    {
+      id: result.fetch(:id),
+      group_title: groups[group_key].fetch(:title),
+      subgroup_title: subgroup_title,
+      support_and_advice: support_and_advice,
+      text: result.fetch(:text),
+      href: result.fetch(:href, ""),
+      show_to_nations: result.fetch(:show_to_nations, []).join(" OR "),
+    }
   end
 end

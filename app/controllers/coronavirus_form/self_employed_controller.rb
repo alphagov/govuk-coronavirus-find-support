a05a7fe731630436b068f3064a154ff027d8df1c
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2020-06-22 09:34:42 +0100

    Refactor logic

--- app/controllers/coronavirus_form/self_employed_controller.rb
+++ app/controllers/coronavirus_form/self_employed_controller.rb
@@ -19,26 +19,23 @@ class CoronavirusForm::SelfEmployedController < ApplicationController
       flash.now[:validation] = invalid_fields
       log_validation_error(invalid_fields)
       render controller_path
-    elsif I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.skip_next_question_options").include? @form_responses[:self_employed]
-      update_session_store
-      session[:questions_to_ask] = remove_questions(%w[have_you_been_made_unemployed are_you_off_work_ill])
-      redirect_path = next_question(controller_name)
-      redirect_path == "results" ? redirect_to_results : redirect_to(polymorphic_url(redirect_path))
-    elsif last_question == controller_name
-      redirect_to_results
     else
       update_session_store
-      session[:questions_to_ask] = add_questions(%w[have_you_been_made_unemployed are_you_off_work_ill], controller_name)
+      write_responses if last_question == controller_name
+      update_questions_to_ask
       redirect_to polymorphic_url(next_question(controller_name))
     end
   end
 
+
 private
 
-  def redirect_to_results
-    update_session_store
-    write_responses
-    redirect_to results_url
+  def update_questions_to_ask
+    session[:questions_to_ask] = if I18n.t("coronavirus_form.groups.being_unemployed.questions.self_employed.skip_next_question_options").include? @form_responses[:self_employed]
+                                   remove_questions(%w[have_you_been_made_unemployed are_you_off_work_ill])
+                                 else
+                                   add_questions(%w[have_you_been_made_unemployed are_you_off_work_ill], controller_name)
+                                 end
   end
 
   def update_session_store
